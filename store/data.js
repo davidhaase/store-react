2277e10307b9d56ee02a4e7f4b22c5ea505f1bab
David Haase <davidhenryhaase@aol.com>
2020-10-22 10:53:26 -0400

    Initial setup of both servers and commit

---
+++ store/data.js
@@ -0,0 +1,14 @@
+module.exports = function() {
+    return {
+        categories : ["Watersports", "Soccer", "Chess"],
+        products : [
+            {id:1 , name:"Polo Ball" , category:"Watersports", description:"Best floating ball", price:80 },
+            {id:2 , name:"Soccer Ball" , category:"Soccer", description:"No-pain black & white", price:40 },
+            {id:3 , name:"Cleats" , category:"Soccer", description:"Metal-cleated toe", price:150 },
+            {id:4 , name:"Waterskis" , category:"Watersports", description:"Nautica", price:340 },
+            {id:5 , name:"Rook" , category:"Chess", description:"Black onyx", price:25 },
+            {id:6 , name:"Knight" , category:"Chess", description:"Black onyx", price:25 }
+        ],
+        orders : []
+    }
+}
